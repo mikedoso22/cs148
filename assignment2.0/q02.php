78773c30bf33b0dfec550899ba6178eaf26b20f3
Mike Cardoso <mcardoso@silk1.uvm.edu>
2015-09-22 10:26:27 -0400

    wow

---
+++ assignment2.0/q02.php
@@ -0,0 +1,32 @@
+<?php
+
+
+include "top.php";
+$query = 'select fldDepartment from tblCourses where fldCourseName like "Introduction%" ';
+$info2 = $thisDatabaseReader->select($query, "", 1, 0, 2, 0, false, false);
+echo count($info2);
+
+echo '<p>'.$query.'</p>';
+print '<table>';
+$columns = 1;
+//now print out each record
+
+$highlight = 0; // used to highlight alternate rows
+foreach ($info2 as $rec) {
+    $highlight++;
+    if ($highlight % 2 != 0) {
+        $style = ' odd ';
+    } else {
+        $style = ' even ';
+    }
+    print '<tr class="' . $style . '">';
+    for ($i = 0; $i < $columns; $i++) {
+        print '<td>' . $rec[$i] . '</td>';
+    }
+    print '</tr>';
+}
+// all done
+print '</table>';
+
+include "footer.php";
+?>
---
+++ assignment2.0/q10.php
@@ -0,0 +1,30 @@
+<?php
+
+
+include "top.php";
+$query = 'select distinct fldBuilding, count(fldNumStudents), fldDays from tblSections where fldDays like "%F%" group by fldBuilding order by count(fldNumStudents) desc ';
+$info2 = $thisDatabaseReader->select($query, "", 1, 1, 2, 0, false, false);
+echo count($info2);
+echo '<p>'.$query.'</p>';
+print '<table>';
+$columns = 3;
+//now print out each record\
+$highlight = 0; // used to highlight alternate rows
+foreach ($info2 as $rec) {
+    $highlight++;
+    if ($highlight % 2 != 0) {
+        $style = ' odd ';
+    } else {
+        $style = ' even ';
+    }
+    print '<tr class="' . $style . '">';
+    for ($i = 0; $i < $columns; $i++) {
+        print '<td>' . $rec[$i] . '</td>';
+    }
+    print '</tr>';
+}
+// all done
+print '</table>';
+
+include "footer.php";
+?>
