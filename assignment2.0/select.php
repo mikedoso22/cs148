32adc90131a2e4068ecfa6908f8d2ad09761fa73
Mike Cardoso <mcardoso@silk1.uvm.edu>
2015-09-22 10:03:39 -0400

    query's in folder

---
+++ assignment2.0/select.php
@@ -0,0 +1,14 @@
+
+<?php
+
+//##############################################################################
+//
+// main home page for the site 
+// 
+//##############################################################################
+include "top.php";
+
+// Begin output
+print "<p>I talked to Bob and he said the query's could be on my nav and it be no problem";
+include "footer.php";
+?>
